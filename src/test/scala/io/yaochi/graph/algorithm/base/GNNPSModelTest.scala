3c57fe999c2d06c08bd0e6db7f1d4063b05b3960
yaochi <yaochi@sugo.io>
2020-01-19 12:17:58 +0800

    update the unit

--- src/test/scala/io/yaochi/graph/algorithm/base/GNNPSModelTest.scala
+++ src/test/scala/io/yaochi/graph/algorithm/base/GNNPSModelTest.scala
@@ -3,6 +3,7 @@ package io.yaochi.graph.algorithm.base
 import com.tencent.angel.conf.AngelConf
 import com.tencent.angel.spark.context.PSContext
 import io.yaochi.graph.dataset.CoraDataset
+import io.yaochi.graph.util.DataLoaderUtils
 import org.apache.spark.{SparkConf, SparkContext}
 import org.junit.{After, Before, Test}
 
@@ -20,7 +21,18 @@ class GNNPSModelTest {
 
   @Test
   def testGNNPSModel(): Unit = {
-    val (edges, features, labels) = CoraDataset.load("data/cora")
+    val (edgeDF, featureDF, labelDF) = CoraDataset.load("data/cora")
+
+    val edges = edgeDF.select("src", "dst").rdd
+      .map(row => (row.getLong(0), row.getLong(1)))
+      .filter(f => f._1 != f._2)
+    val (minId, maxId, numEdges) = edges.mapPartitions(DataLoaderUtils.summarizeApplyOp)
+      .reduce(DataLoaderUtils.summarizeReduceOp)
+    val index = edges.flatMap(f => Iterator(f._1, f._2))
+    println(s"minId=$minId maxId=$maxId numEdges=$numEdges")
+
+    PSContext.getOrCreate(SparkContext.getOrCreate())
+    val psModel = GNNPSModel(minId, maxId, index, 1)
   }
 
   @After
